609ff4f6d12d44b5fb303078699089e7c5686bb0
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-07-20 15:24:08 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['https://mystore-ecommerce-frontend.onrender.com'];
 
 app.use(cors({
   origin: function (origin, callback) {
