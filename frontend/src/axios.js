52b6d8b2cd1d91d67195895ce80c21875644025c
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-07-20 15:19:36 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', 
+  baseURL: 'https://mystore-ecommerce-coj7.onrender.com', 
   withCredentials: true,
 });
 
